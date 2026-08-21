ddc5bfe5eed3adccebb169bcd3d966ad1fc63bee
taherelhares <taheralhares@gmail.com>
2026-08-21 11:02:42 +0300

    chore: scaffold solution and Web API project

---
+++ src/VehicleExplorer.Api/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+if (!app.Environment.IsProduction())
+{
+    app.UseHttpsRedirection();
+}
+
+app.Run();
